296933b9331f142c4bb3b603286ce43ec4ea4235
KarenCampo <1472@holbertonschool.com>
2020-05-02 22:14:04 +0000

    adding task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    index = len(sys.argv) - 1
+    if index == 0:
+        print("0 arguments.")
+    if index == 1: 
+        print(index, "argument:")
+        print("{:d}:".format(index), sys.argv[index], end="")
+        
+    else:
+        print(index, "arguments:")
+        for name in range(index):
+            print("{:d}:".format(name + 1), sys.argv[name + 1], end="")
+
