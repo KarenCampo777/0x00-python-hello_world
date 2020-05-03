68e623eff1626850fd256060f21fe29574ac5bdc
KarenCampo777 <1472@holbertonschool.com>
2020-05-03 02:16:54 +0000

    adding task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 if __name__ == "__main__":
     import sys
     index = len(sys.argv) - 1
-    if index == 0:
+    if index == 1:
         print("0 arguments.")
-    if index == 1: 
+    if index == 2:
         print(index, "argument:")
-        print("{:d}:".format(index), sys.argv[index], end="")
-        
+        print("{:d}:".format(index), sys.argv[index], end='\n')
+
     else:
         print(index, "arguments:")
         for name in range(index):
-            print("{:d}:".format(name + 1), sys.argv[name + 1], end="")
 
+            print("{:d}:".format(name + 1), sys.argv[name + 1], end='\n')
