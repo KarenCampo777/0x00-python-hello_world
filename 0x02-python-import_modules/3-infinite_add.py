a528d85c0232b8302e52f4c02eb9d167b8aa8be7
KarenCampo777 <1472@holbertonschool.com>
2020-05-04 03:05:24 +0000

    adding task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    res = len(sys.argv)
+    sum = 0
+    if res == 0:
+        print"{}".format("0"))
+    else:
+        if res == 2:
+            print("{:s}.format(sys.arv[1]))
