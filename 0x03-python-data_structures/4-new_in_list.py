642e91b1affeeb173b94457eb11a7508fa5630fa
KarenCampo777 <1472@holbertonschool.com>
2020-05-06 00:32:18 +0000

    updating file

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    nlist = my_list.copy()
+    if idx < 0 or idx >= len(nlist):
+        nlist[idx] = element
+        return nlist
+    return my_list
