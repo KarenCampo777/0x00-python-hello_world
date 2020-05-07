4cadad1184a74e30d841baad9ac7e1ec41fda08c
KarenCampo777 <1472@holbertonschool.com>
2020-05-07 03:46:14 +0000

    adding solved task (I hope)

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    n_list = []
+    for i in my_list:
+        if not i % 2:
+            n_list.append(True)
+        else:
+            n_list.append(False)
+    return n_list
