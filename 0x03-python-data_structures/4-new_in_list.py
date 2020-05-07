d42101bf00012dba21ff26d5b87b827832301f25
KarenCampo777 <1472@holbertonschool.com>
2020-05-07 15:03:18 +0000

    reloading task for after many corrections

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     nlist = my_list.copy()
-    if idx < 0 or idx >= len(nlist):
-        nlist[idx] = element
+    if idx < 0 and idx > len(nlist):
         return nlist
-    return my_list
+    nlist[idx] = element
+    return nlist
