3b753edc58d6c92e853b91c32c38abb03c6eeda6
KarenCampo777 <1472@holbertonschool.com>
2020-05-10 04:46:20 +0000

    uploading task 9 with corrections

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-# Function that returns a new dictionary multiplied by 2
-# All values are only integers
     new_dictionary = a_dictionary.copy()
     for key, num in a_dictionary.items():
         new_dictionary[key] = num * 2
