3b25f5e18477bc55f7461a1e7570b63ce277a21a
KarenCampo777 <1472@holbertonschool.com>
2020-05-11 02:52:52 +0000

    adding task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary == {}:
+        return None
+    else:
+        biggest = max(a_dictionary, key=a_dictionary.get)
+        return biggest
