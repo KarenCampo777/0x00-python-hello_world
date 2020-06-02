e071282e4ef463159937e22df9e02737886a858e
KarenCampo777 <1472@holbertonschool.com>
2020-06-02 23:35:14 +0000

    added the 2 mandatory lines and deleted white space for task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" Module that creates a function that returns
-the list of available attributes and methods of an object"""
+""" This module creates a function that returns
+    list of available attributes and methods of an object"""
+
+
 def lookup(obj):
     """returns
 the list of available attributes and methods of an object"""
