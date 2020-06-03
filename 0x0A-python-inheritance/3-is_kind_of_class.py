3d118a004e7d3a8f1729bd95c6b9e73a9c7cbaf5
KarenCampo777 <1472@holbertonschool.com>
2020-06-03 01:15:56 +0000

    adding task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns True if the object is an instance; or False."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Verifies if an object is an instance class"""
+    return isinstance(obj, a_class)
