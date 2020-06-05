715e67f57ca73bd2f060c730345dcc6aad5a1cfc
KarenCampo777 <1472@holbertonschool.com>
2020-06-05 01:49:40 +0000

    adding task 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename, encoding="UTF-8") as file:
+        counter = 0
+        for line in file:
+            counter += 1
+            if (nb_lines <= 0) or (nb_lines < counter):
+                print(line, end="")
