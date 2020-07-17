0112a54050cddd069c88ee3811ce6f92a1ddd8c1
KarenCampo777 <1472@holbertonschool.com>
2020-07-17 00:11:17 +0000

    a simple correction

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
---This script List all records of the second_table table ordered by score
+-- This script List all records of the second_table table ordered by score
 SELECT score, name FROM second_table ORDER BY score DESC;
