919a2df6d03ec29c7abbf5f8e07117ce7f6cf3af
KarenCampo777 <1472@holbertonschool.com>
2020-12-15 16:22:18 +0000

    proram tested and running gracefully

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    "Lists tatates from the db"
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    exe = cur.execute("""SELECT * FROM
+                      states WHERE name LIKE BINARY "{}" ORDER BY id ASC""" .format(argv[4]))
+    content = cur.fetchall()
+    for b in content:
+        print(b)
+    cur.close()
+    db.close()
