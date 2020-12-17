b379d6fe616af7d7a8337863162b5dd5d15e3d78
KarenCampo777 <1472@holbertonschool.com>
2020-12-17 22:33:52 +0000

    fixed white space

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     from sqlalchemy.orm import sessionmaker
     from sys import argv
     import sys
-   
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(sys.argv[1], sys.argv[2],
                                   sys.argv[3]), pool_pre_ping=True)
