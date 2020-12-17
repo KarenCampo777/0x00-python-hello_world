157219f3bc1afe9b9ac52592ccfed7b3347a0744
KarenCampo777 <1472@holbertonschool.com>
2020-12-17 21:38:09 +0000

    fixed white spaces requirements

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,7 +11,6 @@ class State(Base):
 
     __tablename__ = "states"
 
-
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
     name = Column(String(128), nullable=False)
