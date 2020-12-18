278a36d4a74ec9bf89a4a0d0df266165fabd96be
KarenCampo777 <1472@holbertonschool.com>
2020-12-18 05:02:44 +0000

    working nice and smooth

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+   Script that changes the name of a State object from the database
+"""
+if __name__ == "__main__":
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    import sys
+
+    # Creates the engine we need
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name = State(name="Louisiana")
+    session.add(state_name)
+    session.commit()
+    print(state_name.id)
+    session.close()
