d523356f7225e54fe7573a9828b64cd7df0b42dc
KarenCampo777 <1472@holbertonschool.com>
2021-01-14 03:45:14 +0000

    now the program works

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     intra = "https://intranet.hbtn.io/status"
     res = requests.get(intra)
     print("Body response:")
-    print("\t- type: {}".format(type(intra.text)))
-    print("\t- content: {}".format(intra.text))
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
