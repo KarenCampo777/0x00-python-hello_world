53bb865f4af10291b576f8557ba989c5bd5ba751
KarenCampo777 <1472@holbertonschool.com>
2021-02-19 21:39:31 +0000

    101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-// Function that executes x times a function.
+// Function that executes x times a function
 
 exports.callMeMoby = function (num, theFunction) {
-
   for (let i = 0; i < num; i++) {
     theFunction();
   }
