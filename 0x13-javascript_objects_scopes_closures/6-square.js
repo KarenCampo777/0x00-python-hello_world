6989ce93ecdcf0253ce487dca321645e8b8a5bfa
KarenCampo777 <1472@holbertonschool.com>
2021-02-20 01:12:45 +0000

    fixed an error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // C is fun?
-Class Square extends require('./5-square') {
+class Square extends require('./5-square') {
   charPrint (obj) {
     if (obj === undefined) {
       obj = 'X';
