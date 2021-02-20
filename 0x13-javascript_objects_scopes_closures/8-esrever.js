34469482afa129167ed3e6c9458d2a769cbb2fe4
KarenCampo777 <1472@holbertonschool.com>
2021-02-20 00:58:50 +0000

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+//revese
+exports.esrever = function (list) {
+  const list2 = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    list2.push(list[i]);
+  }
+  return list2;
+};
