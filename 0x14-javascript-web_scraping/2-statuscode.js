a8fe2523e543b0924988298cc0649bed6c02563a
KarenCampo777 <1472@holbertonschool.com>
2021-02-22 21:48:20 +0000

    script that display the status code of a GET request.

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Script that display the status code of a GET request.
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) throw error;
+  console.log('code: ' + response.statusCode);
+});
